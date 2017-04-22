958478669d7c31a1c27b54257842d0cc6f4ca004
Daniel H da Silva <danielhensilva@gmail.com>
2017-04-22 18:07:52 -0300

    add toastr

--- 2017/angular2-intro/app/app.module.ts
+++ 2017/angular2-intro/app/app.module.ts
@@ -4,6 +4,8 @@ import {EventsAppComponent} from './events-app.component';
 import {EventsListComponent} from './events/events-list.component';
 import {EventThumbnailComponent} from './events/event-thumbnail.component';
 import {NavBarComponent} from './nav/navbar.component';
+import {EventService} from './events/shared/event.service';
+import {ToastrService} from './common/toastr.service';
 
 @NgModule({
   imports: [BrowserModule],
@@ -12,6 +14,10 @@ import {NavBarComponent} from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
+  ],
+  providers: [
+    EventService,
+    ToastrService
   ],
   bootstrap: [EventsAppComponent]
 })
