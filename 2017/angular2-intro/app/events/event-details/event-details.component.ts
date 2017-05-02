373ad48ef1f98273d47f7707187ee3352174394b
Daniel H da Silva <danielhensilva@gmail.com>
2017-05-01 23:23:15 -0300

    links

--- 2017/angular2-intro/app/events/event-details/event-details.component.ts
+++ 2017/angular2-intro/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: '/app/events/event-details/event-details.component.html',
@@ -12,11 +13,14 @@ export class EventDetailsComponent {
 
   event:any;
 
-  constructor(private eventService:EventService) {
+  constructor(
+    private eventService:EventService,
+    private route:ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    const id:number = +this.route.snapshot.params['id'];
+    this.event = this.eventService.getEvent(id);
   }
 
 }
