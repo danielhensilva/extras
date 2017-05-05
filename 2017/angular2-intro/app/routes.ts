aa7864c8f7f5cef4c5ddbf5bb5416b16e816ff8b
Daniel H da Silva <danielhensilva@gmail.com>
2017-05-05 01:33:37 -0300

    add user module

--- 2017/angular2-intro/app/routes.ts
+++ 2017/angular2-intro/app/routes.ts
@@ -12,4 +12,5 @@ export const appRoutes : Routes = [
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ]
