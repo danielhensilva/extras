0014419912ed9ec60fe80de5cd3db11b2f0eed83
Daniel H da Silva <danielhensilva@gmail.com>
2017-05-05 22:53:25 -0300

    login form

---
+++ 2017/angular2-intro/app/user/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'app/user/login.component.html'
+})
+export class LoginComponent {
+
+  login(formValues) {
+    console.log(formValues);
+  }
+
+}
