8f2f0c9e03e3354d016bc137673722ce1b2d5a86
Daniel H da Silva <danielhensilva@gmail.com>
2017-05-19 00:09:03 -0300

    validators

--- 2017/angular2-intro/app/user/profile.component.ts
+++ 2017/angular2-intro/app/user/profile.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
-  templateUrl: `app/user/profile.component.html`
+  templateUrl: `app/user/profile.component.html`,
+  styleUrls: ['app/user/profile.component.css']
+
 })
 export class ProfileComponent implements OnInit {
 
   public profileForm: FormGroup;
+
+  private firstName: FormControl;
+  private lastName: FormControl;
+
   private authService: AuthService;
   private router: Router;
 
@@ -18,17 +24,54 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
+    const validators = [
+      Validators.required,
+      Validators.pattern('[a-zA-Z].*')
+    ];
+
+    this.firstName = new FormControl(this.authService.currentUser.firstName, validators);
+    this.lastName = new FormControl(this.authService.currentUser.lastName, validators);
+
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
   saveProfile(formValues) {
+    if (!this.profileForm.valid) {
+      return;
+    }
+
     this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
     this.router.navigate(['events']);
+  }
+
+  validateField(field: FormControl, type: string) {
+    if (field.untouched) {
+      return true;
+    }
+    if (field.valid) {
+      return true;
+    }
+    if (!type) {
+      return true;
+    }
+    if (!field.errors) {
+      return true;
+    }
+    if (!field.errors[type]) {
+      return true;
+    }
+    return false;
+  }
+
+  validateFirstName(type: string) {
+    return this.validateField(this.firstName, type);
+  }
+
+  validateLastName(type: string) {
+    return this.validateField(this.lastName, type);
   }
 
   cancel() {
