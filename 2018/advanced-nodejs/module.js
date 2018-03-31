9b8b7fec17a27987236315d70795a29f8bd84e97
Daniel H da Silva <danielhensilva@gmail.com>
2018-03-31 23:34:12 +0100

    update

--- 2018/advanced-nodejs/module.js
+++ 2018/advanced-nodejs/module.js
@@ -1,4 +1,35 @@
+// Module package
 console.log(module);
 
+// Seeing module wrapper function with (exports, require, module) parameters
+console.log(require("module").wrapper);
+
+
+// Consuming addons packages
+// check addon folder
 const addon = require("addon");
 console.log(addon.hello());
+
+
+// Trick to export as function or as command line app
+
+const print = (header) => {
+    console.log('*'.repeat(header.length));
+    console.log(header);
+    console.log('*'.repeat(header.length));
+};
+
+if (require.main == module) {
+    // Running as a script
+    // -> Calling function directly
+    print(process.argv[2]);
+}
+else {
+    // Being required
+    // -> exporting function
+    module.exports = {print};
+}
+
+// Console
+//  node module 'Daniel' 
+//  node -p "require('./module').print('Daniel')"
