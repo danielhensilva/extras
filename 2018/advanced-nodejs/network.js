baf62dec98fb55a524d3b682515d2e9b61c16cb5
Daniel H da Silva <danielhensilva@gmail.com>
2018-04-01 22:16:59 +0100

    update

---
+++ 2018/advanced-nodejs/network.js
@@ -0,0 +1,19 @@
+const net = require('net')
+const server = net.createServer()
+
+server.on('connection', socket => {
+    console.log('Client connected...')
+    socket.write('Welcome new client!\n')
+
+    socket.on('data', data => {
+        console.log('data is ', data);
+        socket.write('data is ');
+        socket.write(data);
+    })
+
+    socket.on('end', () => {
+        console.log('Client disconnected...')
+    })
+})
+
+server.listen(8000, () => console.log('Server bound...'));
