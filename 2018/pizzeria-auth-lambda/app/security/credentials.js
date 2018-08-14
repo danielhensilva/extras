a3ebb955bcda9c3fe954ee48c945163fff3452ae
Daniel H da Silva <danielhensilva@gmail.com>
2018-08-14 23:12:36 +0100

    update

--- 2018/pizzeria-auth-lambda/app/security/credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  AWS: {
-    Cognito: {
-      UserPoolId: "eu-west-1_03wwNPfHA",
-      ClientId: "2289kcpq54s6s1vprqova9jknr"
-    }
-  }
-};
--- 2018/pizzeria-auth-lambda/app/security/credentials.sample.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  AWS: {
-    Cognito: {
-      UserPoolId: "",
-      ClientId: ""
-    }
-  }
-};
