7f2c71b02560da51f47f761701a017d9a76c2997
Daniel H da Silva <danielhensilva@gmail.com>
2019-03-14 08:01:40 +0000

    Update

--- 2019/flask/flaskr/__init__.py
+++ 2019/flask/flaskr/__init__.py
@@ -24,15 +24,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
