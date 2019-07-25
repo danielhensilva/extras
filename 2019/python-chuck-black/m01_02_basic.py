ebed5a9ee20b1a2be9f09ab1c7430c2a749e9803
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-25 21:51:47 +0100

    Update

---
+++ 2019/python-chuck-black/m01_02_basic.py
@@ -0,0 +1,5 @@
+balance = 1000.00
+new_balance = balance
+balance += 250
+print('balance object id:', id(balance), '\tvalue:', balance)
+print('new_balance object id:', id(new_balance), '\tvalue:', new_balance)
