e114b2e95a5de23f45d430afdac1400e020217bf
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-25 21:51:51 +0100

    Update

---
+++ 2019/python-chuck-black/m01_lab.py
@@ -0,0 +1,19 @@
+name = 'Fred Flinstone'
+location = 'Bedrock Area'
+
+relationship_status = 'Married'
+relationship_spouse_name = 'Wilma Flinstone'
+
+work_employer = 'Slate Rock and Gravel'
+work_occupation = 'Bronto Crane Operator'
+work_income = '36000.0'
+
+print('Name:', name)
+print('Location:', location)
+print('')
+print('Relationship status:', relationship_status)
+print('--- Spouse:', relationship_spouse_name)
+print('')
+print('Employed by:', work_employer)
+print('--- Occupation:', work_occupation)
+print('--- Income:', work_income)
