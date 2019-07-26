efeb7b263d065ccb893cc173c7dca6c5eda90d81
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-26 09:01:50 +0100

    Update

--- 2019/python-chuck-black/m03_01_csv.py
+++ 2019/python-chuck-black/m03_01_csv.py
@@ -1,4 +1,18 @@
-with open('m03_01_csv.txt', 'r') as bills:
-    for bill in bills:
-        entries = bill.strip().split(',')
-        print(f'{(entries[0]):3} {entries[1]:20} {entries[2]:30} {entries[3]:10}')
+bills = []
+file_lines = list(open('m03_01_csv.txt', 'r'))
+file_titles = file_lines[0].strip().split(',')
+file_entries = list(map(lambda x: x.strip().split(','), file_lines[1:]))
+file_columns_count = len(file_titles)
+
+
+for file_entry in file_entries:
+    bill = dict()
+    for i in range(file_columns_count):
+        title = file_titles[i]
+        value = file_entry[i]
+        bill[title] = value
+    bills.append(bill)
+
+
+for bill in bills:
+    print(bill)
