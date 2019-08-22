298de3c40c294437591fd08fbef43029b86e707e
Daniel H da Silva <danielhensilva@gmail.com>
2019-08-22 21:58:43 +0100

    Update

---
+++ 2019/java-deitel/src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    public static void Main(String[] args) {
+        Account account = new Account();
+        account.setName("New name");
+        System.out.println(account.getName());
+    }
+
+}
---
+++ 2019/java-deitel/test/AccountTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class AccountTests {
+
+    @Test
+    void shouldWork() {
+
+    }
+
+}
