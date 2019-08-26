9e67992daa7ed4af7570cf95e24bc71207ccaffb
Daniel H da Silva <danielhensilva@gmail.com>
2019-08-26 06:47:18 +0100

    Update

--- 2019/java-spring-boot/src/main/java/com/oreilly/demo/DemoApplication.java
+++ 2019/java-spring-boot/src/main/java/com/oreilly/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.oreilly.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public NumberFormat defaultCurrencyFormat() {
+		return NumberFormat.getCurrencyInstance();
+	}
+
+	@Bean
+	public NumberFormat germanCurrencyFormat() {
+		return NumberFormat.getCurrencyInstance(Locale.GERMANY);
 	}
 
 }
--- 2019/java-spring-boot/src/test/java/com/oreilly/demo/DemoApplicationTests.java
+++ 2019/java-spring-boot/src/test/java/com/oreilly/demo/DemoApplicationTests.java
@@ -2,15 +2,46 @@ package com.oreilly.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.NumberFormat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Autowired
+	@Qualifier("defaultCurrencyFormat")
+	private NumberFormat formatter;
+
+	@Test
+	public void defaultCurrency() {
+		double amount = 123456.901234;
+		System.out.println(formatter.format(amount));
+	}
+
+	@Test
+	public void germanCurrency() {
+		double amount = 123456.901234;
+		NumberFormat deutschFormatter = ctx.getBean("germanCurrencyFormat", NumberFormat.class);
+		System.out.println(deutschFormatter.format(amount));
+	}
+
 	@Test
 	public void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println(String.format("There are %d beans in the application context", count));
+
+		for (String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
