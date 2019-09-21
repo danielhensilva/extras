ef67abbe81f15f660b5ac3fb3c33b117084f1191
Daniel H da Silva <danielhensilva@gmail.com>
2019-09-21 20:44:33 +0100

    Update

--- 2019/java-play/app/controllers/Application.java
+++ 2019/java-play/app/controllers/Application.java
@@ -2,13 +2,15 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+    public Result index(final String name) {
+        if (name == null || name.isEmpty()) {
+            return ok("Hello, world");
+        }
+        return ok(hello.render(name));
     }
 
 }
---
+++ 2019/warehouse/app/controllers/Products.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Products extends Controller {
+
+    public Result list() {
+        return TODO;
+    }
+
+    public Result create() {
+        return TODO;
+    }
+
+    public Result details(String ean) {
+        return TODO;
+    }
+
+    public Result update() {
+        return TODO;
+    }
+
+}
