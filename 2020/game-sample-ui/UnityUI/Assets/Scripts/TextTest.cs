ea0593220a6f8c493521de61500319fe4a077ea5
Daniel H da Silva <danielhensilva@gmail.com>
2020-09-01 18:59:06 +0100

    Update

---
+++ 2020/game-sample-ui/UnityUI/Assets/Scripts/TextTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTest : MonoBehaviour {
+
+    public string message;
+    private Text textInstance;
+
+    void Start() {
+        textInstance = GetComponent<Text>();
+    }
+
+    void Update() {
+        textInstance.text = message;
+    }
+}
